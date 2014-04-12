7a3edfc1c8dbc79b809abae0edc45538f15322c7
Jonas Kindler <kontakt@jonaskindler.de>
2014-04-12 14:34:19 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CogsciRSS::Application.config.secret_key_base = 'ed386bf002460f26ace4e024ffbd70d93382f1c9df6a438c6fa25dc63840cfe92be8dd4f442c1a0ed80b7416541be22c1b9e1a19fbd3fc9702570909eb4ebb6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CogsciRSS::Application.config.session_store :cookie_store, key: '_CogsciRSS_session'
