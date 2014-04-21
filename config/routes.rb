f5f72bb3da341d003844f34b70da69d988867764
jkirchner <kirchner.jan@icloud.com>
2014-04-21 18:26:29 +0200

    added first view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CogsciRSS::Application.routes.draw do
+  get "feeds/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
